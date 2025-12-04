ab1cc9c767d1d62de79afa3d02c58ad43f12b7b0
natalie-03 <112846166+natalie-03@users.noreply.github.com>
2025-12-04 12:21:44 +0800

    Add files via upload

---
+++ js/f.js
@@ -0,0 +1,21 @@
+// f.js - 面試結果統計資料
+window.interviewResults = [
+  {
+    "label": "錄取",
+    "value": 36
+  },
+  {
+    "label": "未錄取",
+    "value": 26
+  },
+  {
+    "label": "無聲卡",
+    "value": 0
+  },
+  {
+    "label": "等待中",
+    "value": 1
+  }
+];
+
+// 如果需要在其他檔案中使用，可以通過 window.interviewResults 存取
